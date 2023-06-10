e165b661a76ae5a5431190041ff1cbe3b06bada8
Wendel Gomes <wendelgomesdh7@gmail.com>
2023-06-10 12:58:27 -0400

    Relatorios com cypress

--- cypress.config.js
+++ cypress.config.js
@@ -6,4 +6,12 @@ module.exports = defineConfig({
       // implement node event listeners here
     },
   },
+  reporter: 'mochawesome',
+  reporterOptions: {
+    reportDir: 'cypress/relatorios',
+    overwrite: true,
+    html: true,
+    json: false,
+    timestemp: 'ddmmyyyy_HHMMss'
+  }
 });
