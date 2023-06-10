3b0a78b9b3a944122455a0bec68e25858434bcae
Wendel Gomes <wendelgomesdh7@gmail.com>
2023-06-10 13:51:20 -0400

    Teste com cypress

--- cypress/e2e/ebac_teste/home.cy.js
+++ cypress/e2e/ebac_teste/home.cy.js
@@ -1,54 +1,40 @@
-/// <reference types="cypress" />
+/// <reference types='cypress'>
+describe("Teste de Automação com Cypress", () => {
+  beforeEach(() => {
+    cy.visit("https://agenda-contatos-react.vercel.app/");
+  });
 
-describe('Teste de Automação com Cypress', () => {
-    it('Adicionar Contato', () => {
-        cy.visit('https://agenda-contatos-react.vercel.app/');
-    
-        cy.get('[type="text"]').type('João');
-        cy.get('[type="email"]').type('joao@email.com');
-        cy.get('[type="tel"]').type('11987654321');
-        cy.contains('Adicionar').click();
-    });
+  it("Adicionar contato", () => {
+    cy.get("input").eq(0).type("Wendel Gomes");
+    cy.get("input").eq(1).type("wendel@teste.com");
+    cy.get("input").eq(2).type("(11) 9999-9999");
+    cy.get(".adicionar").click();
 
-    it('Editar Contato', () => {
-        cy.visit('https://agenda-contatos-react.vercel.app/');
+    // Capturar screenshot
+    cy.screenshot("teste_inclusao_contato");
+  });
 
-        // Certificar-se de que o contato existe na lista antes de editar
-        cy.contains('João').should('exist');
+  it("Editar contato", () => {
+    cy.get(":nth-child(2) > .sc-gueYoa > .edit").click();
+    cy.get('[type="text"]').clear().type("Gian Souza");
+    cy.get('[type="email"]').clear().type("gian@teste.com");
+    cy.get('[type="tel"]').clear().type("(11) 1828-8888");
+    cy.get(".alterar").click();
 
-        // Localizar o contato desejado na tabela (assumindo que o contato já exista)
-        cy.get('[data-id="edit"]').within(() => {
-          // Clicar no botão de edição do contato
-            cy.get('button[class="edit"]').click();
-        });
+    // Capturar screenshot
+    cy.screenshot("teste_alteracao_contato");
+  });
 
-        // Preencher os campos do formulário de edição
-        cy.get('[type="text"]').clear().type('João Silva');
-        cy.get('[type="email"]').clear().type('joao.silva@email.com');
-        cy.get('[type="tel"]').clear().type('999999999');
+  it("Remover contato", () => {
+    cy.get(":nth-child(2) > .sc-gueYoa > .delete").click();
 
-        // Clicar no botão de salvar a edição
-        cy.contains('Salvar').click();
+    // Capturar screenshot
+    cy.screenshot("teste_remocao_contato");
+  });
 
-        // Verificar se a edição do contato foi realizada corretamente
-        cy.contains('Contato atualizado com sucesso!');
-    });
-
-
-    it('Remover Contato', () => {
-        cy.visit('https://agenda-contatos-react.vercel.app/');
-
-      // Certificar-se de que o contato existe na lista antes de remover
-        cy.contains('João').should('exist');
-
-      // Localizar o contato desejado na tabela (assumindo que o contato já exista)
-        cy.contains('João').parent('tr').within(() => {
-        // Clicar no botão de exclusão do contato
-        cy.get('button[class="delete"]').click();
-    });
-
-      // Verificar se o contato foi removido corretamente
-        cy.contains('Contato removido com sucesso!');
-        cy.contains('João').should('not.exist');
-    });
+  it("Finalizando os testes", () => {
+    expect(true).to.be.true;
+    // Capturar screenshot
+    cy.screenshot("teste_finalizado");
+  });
 });
